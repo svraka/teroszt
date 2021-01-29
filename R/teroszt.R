svraka/teroszt

commit 0ca2c59dbf4984b25d2b4ccff770db0de5d760d6

Removed build dependency on janitor
We used it only for the `clean_names()` function to automatically
clean column names in Excel files. However, its results can change,
therefore we explicitly name all columns while reading Excel-files.

data-raw/irsz_2018.R
@@ -3,8 +3,6 @@ library(stringr)
 library(purrr)
 library(readxl)
 library(tidyr)
-library(janitor, warn.conflicts = FALSE)
-
 
 
 # Postal code from the Post Office
@@ -13,8 +11,11 @@ irsz_posta_file <- "data-raw/iranyitoszam_2019-01-15.xlsx"
 
 # Regular postal codes
 
-irsz_posta_2018_sima <- read_excel(irsz_posta_file, col_types = "text") %>%
-  clean_names() %>%
+irsz_posta_2018_sima <-
+  read_excel(irsz_posta_file,
+             col_names = c("irsz", "telepules", "telepulesresz"),
+             skip = 2,
+             col_types = "text") %>%
   mutate(telepules = str_remove(telepules, "\\*"))
 
 # Postal codes based on street addresses (for large cities)
@@ -25,10 +26,11 @@ irsz_posta_2018_utcajegyzekbol <- excel_sheets(irsz_posta_file) %>%
   str_subset(re_utca) %>%
   set_names() %>%
   map_dfr(
-    ~read_excel(irsz_posta_file, sheet = ., col_types = "text") %>% clean_names,
+    ~ read_excel(irsz_posta_file, sheet = ., col_types = "text"),
     .id = "telepules"
   ) %>%
-  select(telepules, irsz, ker) %>%
+  select(telepules, IRSZ, KER) %>%
+  rename_with(.fn = tolower, .cols = everything()) %>%
   distinct() %>%
   mutate(
     telepules = str_remove(telepules, re_utca),
@@ -64,13 +66,8 @@ irsz_postahivatalok <-
     path = here::here("data-raw", "Allando_postai_szolgaltatohelyek.xlsx"),
     guess_max = 10000
   ) %>%
-  clean_names() %>%
-  select(
-    telepules = x_a_telepules,
-    nev = x_b_postai_szolgaltatohely_megnevezese_illetve_a_szolgaltatas_ellatasanak_modja,
-    irsz = x_c_iranyitoszam,
-    cim = x_d_kozelebbi_cim
-  ) %>%
+  select(1, 3, 4, 5) %>%
+  set_names(c("telepules", "nev", "irsz", "cim")) %>%
   mutate(irsz = as.character(as.integer(irsz)),
          telepules = if_else(str_detect(telepules, "Budapest"),
                              convert_kerulet(telepules), telepules),
@@ -110,10 +107,10 @@ hnt_irsz_2018_kieg <-
   read_excel(
     path = "data-raw/hnt_2018.xls",
     sheet = "Megj. postai ir. számhoz",
+    col_names = c("torzsszam", "megjegyzes_a_postai_iranyitoszamhoz"),
+    skip = 1,
     col_types = "text"
   ) %>%
-  clean_names() %>%
-  rename(torzsszam = helyseg_ksh_kod) %>%
   mutate(
     torzsszam = str_pad(torzsszam, width = 5, pad = "0")
   ) %>%

DESCRIPTION
@@ -21,7 +21,6 @@ Imports:
 Suggests:
     dplyr,
     forcats,
-    janitor,
     readxl,
     tidyr,
     usethis

data-raw/hnt_telepulesreszek_2018.R
@@ -3,7 +3,6 @@ library(readxl)
 library(readr)
 library(stringr)
 library(forcats)
-library(janitor, warn.conflicts = FALSE)
 
 
 # Lookup table for the type of non--built-up areas
@@ -11,12 +10,9 @@ library(janitor, warn.conflicts = FALSE)
 hnt_telepulesreszek_2018_kulterulet_jellege <- read_excel(
     path = "data-raw/hnt_2018.xls",
     sheet = "Külterület települési jellege",
+    col_names = c("kulterulet_jellege", "kulterulet_jellege_nev"),
+    skip = 1,
     col_types = "text"
-  ) %>%
-  clean_names() %>%
-  rename(
-    kulterulet_jellege = rovidites,
-    kulterulet_jellege_nev = kulterulet_telepulesi_jellege,
   ) %>%
   mutate(
     kulterulet_jellege_nev = str_to_sentence(kulterulet_jellege_nev)
@@ -31,25 +27,20 @@ hnt_telepulesresz_jelleg <- read_csv(
 
 # Reading main table
 
-hnt_telepulesreszek_2018 <- read_excel(
+hnt_telepulesreszek_2018 <-
+  read_excel(
     path = "data-raw/hnt_2018.xls",
     sheet = "Településrészek 2018. 01. 01.",
+    col_names = c("torzsszam", "telepules", "megye_kodja",
+                  "telepulesresz", "telepulesresz_jelleg", "irsz",
+                  "kulterulet_jellege",
+                  "telepulesresz_tavolsaga_kozponti_belterulettol",
+                  "nepszamlalasi_lakonepesseg", "lakasok_szama",
+                  "lakott_egyeb_lakoegysegek_szama"),
+    skip = 1,
     col_types = "text"
   ) %>%
-  clean_names() %>%
-  select(-megye_kodja) %>%
-  rename(
-    torzsszam = helyseg_ksh_kod,
-    telepules = helyseg_hivatalos_megnevezese,
-    telepulesresz = telepulesresz_megnevezese,
-    telepulesresz_jelleg = telepulesresz_jelleg_kod,
-    irsz = postai_iranyito_szam,
-    kulterulet_jellege = a_kulterulet_telepulesi_jellege,
-    telepulesresz_tavolsaga_kozponti_belterulettol = a_telepulesresz_tavolsaga_a_kozponti_belterulettol,
-    nepszamlalasi_lakonepesseg = a_nepszamlalasi_lakonepesseg,
-    lakasok_szama = a_lakasok_szama,
-    lakott_egyeb_lakoegysegek_szama = lakott_egyeb_lakoegysegek_szama
-  )
+  select(-megye_kodja)
 
 
 # Cleaning main table

data-raw/tsz_2018.R
@@ -1,23 +1,46 @@
 library(dplyr, warn.conflicts = FALSE)
 library(tidyr)
 library(readxl)
-library(janitor, warn.conflicts = FALSE)
 
 
 # Main table
 
 tsz_2018 <- read_excel(
-    path = "data-raw/teruleti_szamjelrendszer_struktura_elemei_2018.xlsx"
-  ) %>%
-  clean_names()
+  path = "data-raw/teruleti_szamjelrendszer_struktura_elemei_2018.xlsx",
+  col_names = c("telepules_azonosito_torzsszam", "nev",
+                "teruleti_jelzoszam", "jogallas_2005",
+                "statisztikai_nagyregio_kodja", "regio_2016",
+                "nuts_16", "jaras_kod", "jaras_kozpont_kodja",
+                "agglomeracio_kodja",
+                "polgarmesteri_hivatal_kozos_onkormanyzati_hivatal_kodja",
+                "polgarmesteri_hivatal_kozos_onkormanyzati_hivatal_szekhelyenek_kodja",
+                "mezogazdasagi_tajkorzet", "borregio", "borvidek_09",
+                "turisztikai_regio", "vilagoroksegi_helyszinek",
+                "vilagoroksegi_helyszinek_vedoovezete",
+                "nemzeti_parkok",
+                "falusi_szallasadas_lehetseges_helyszinei",
+                "kedvezmenyezett_telepulesek_kodja",
+                "kedvezmenyezett_jarasok_kodja",
+                "szabad_vallalkozasi_zonak",
+                "teruletfejlesztesi_szempontbol_kiemelt_terseg"),
+  skip = 1
+)
 
 
 # Helper table with the main classifications' labels
 
-tsz_2018_megnevezessel <- read_excel(
-    path = "data-raw/teruleti_szamjelrendszer_struktura_elemei_2018_megnevezesekkel.xlsx"
+tsz_2018_megnevezessel <-
+  read_excel(
+    path = "data-raw/teruleti_szamjelrendszer_struktura_elemei_2018_megnevezesekkel.xlsx",
+    col_names = c("telepules_azonosito_torzsszam_telepuleskod", "nev",
+                  "teruleti_jelzoszam",
+                  "teruleti_jelzoszambol_kepzett_megyekod", "megyenev",
+                  "jogallas_2005", "jogallas_2005_megnevezese",
+                  "statisztikai_nagyregio_kodja",
+                  "statisztikai_nagyregio_neve", "regio_2016_kodja",
+                  "regio_neve", "jaras_kod", "jaras_neve"),
+    skip = 1
   ) %>%
-  clean_names() %>%
   # Uniform names
   rename(
     telepules_azonosito_torzsszam = telepules_azonosito_torzsszam_telepuleskod

commit b18edf9f73f6d45fd19f8cb6f435ad1f046b410e

Fix issue about street address level postal codes introduced in 0ca2c59

R/teroszt.R
@@ -69,7 +69,7 @@ utils::globalVariables("irsz_2018")
 #' codes (\sQuote{\emph{ir}ányító\emph{sz}ám}) and the HCSO's
 #' settlement IDs.
 #'
-#' @format A tibble with 4,395 rows, which gives a crosswalk between
+#' @format A tibble with 4,405 rows, which gives a crosswalk between
 #'   all the valid postal codes in the Hungarian Postal Service's
 #'   database, and settlement IDs for all settlements that share a
 #'   postal code.
@@ -85,7 +85,7 @@ utils::globalVariables("irsz_2018")
 #'     and \link{tsz_2018} is ambiguous. Most overlaps happen in
 #'     parts of settlements with low-populations in non-central, or
 #'     even non--build-up areas. This column contains a unique settlement ID
-#'     for each postal code belonging to settlement most likely to be
+#'     for each postal code belonging to the settlement most likely to be
 #'     representative of that postal code. Classification is
 #'     based on borough types (preferring central over, non-central)
 #'     and the boroughs' population sizes.}

data-raw/irsz_2018.R
@@ -26,10 +26,16 @@ re_utca <- "[\\ \\.]u\\."
 irsz_posta_2018_utcajegyzekbol <- excel_sheets(irsz_posta_file) %>%
   str_subset(re_utca) %>%
   set_names() %>%
-  map_dfr(
-    ~ read_excel(irsz_posta_file, sheet = ., col_types = "text"),
-    .id = "telepules"
-  ) %>%
+  map(
+    ~ read_excel(irsz_posta_file, sheet = ., col_types = "text") %>%
+      # We need to do some gymnastics with column names. They differ for some
+      # cities (sometimes there's a period at the end) but we cannot set names
+      # using the `col_names` argument because the number of columns is not
+      # identical on all sheets. Budapest has an extra column for districts
+      # (which we need) and some other cities have empty columns at the end.
+      rename_with(~ str_remove(.x, "\\.$"), everything()),
+    ) %>%
+  bind_rows(.id = "telepules") %>%
   select(telepules, IRSZ, KER) %>%
   rename_with(.fn = tolower, .cols = everything()) %>%
   distinct() %>%

man/irsz_2018.Rd
@@ -5,7 +5,7 @@
 \alias{irsz_2018}
 \title{Postal codes and settlements (2018)}
 \format{
-A tibble with 4,395 rows, which gives a crosswalk between
+A tibble with 4,405 rows, which gives a crosswalk between
   all the valid postal codes in the Hungarian Postal Service's
   database, and settlement IDs for all settlements that share a
   postal code.
@@ -21,7 +21,7 @@ A tibble with 4,395 rows, which gives a crosswalk between
     and \link{tsz_2018} is ambiguous. Most overlaps happen in
     parts of settlements with low-populations in non-central, or
     even non--build-up areas. This column contains a unique settlement ID
-    for each postal code belonging to settlement most likely to be
+    for each postal code belonging to the settlement most likely to be
     representative of that postal code. Classification is
     based on borough types (preferring central over, non-central)
     and the boroughs' population sizes.}